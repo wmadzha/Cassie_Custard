d595b4b1087c978120c971d8e8b8a0e80e3e9582
Wan <wmadzha11@hotmail.com>
2020-02-01 21:47:27 +0800

    Add DI Example

---
+++ CassieCustard.Builder/CassieCustardBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using CassieCustard.Template.DataAccess;
+using CassieCustard.Cassandra.Core;
+namespace CassieCustard.Builder
+{
+    public static class CaseyCake
+    {
+        public static IServiceCollection SetupCassieCustard(this IServiceCollection svc, IConfiguration config)
+        {
+            CassandraSettings noSqlDb = new CassandraSettings()
+            {
+                CassandraPort = 10350,
+                CassandraContactPoint = config.GetSection("CassandraContactPoint_ModuleName").Value,
+                KeySpaceName = config.GetSection("KeySpaceName_ModuleName").Value,
+                Password = config.GetSection("Password_Cassandra_ModuleName").Value,
+                UserName = config.GetSection("UserName_Cassandra_ModuleName").Value
+            };
+            svc.AddScoped(noSqldb => new SampleModuleContext(noSqlDb));
+            return svc;
+        }
+    }
+}
