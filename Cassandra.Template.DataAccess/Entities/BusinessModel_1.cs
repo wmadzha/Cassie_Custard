23549212887a5913058fcec9f5d402d7aeedf0fe
Wan <wmadzha11@hotmail.com>
2020-02-01 21:37:43 +0800

    Add Sample Data Access Examle

---
+++ Cassandra.Template.DataAccess/Entities/BusinessModel_1.cs
@@ -0,0 +1,15 @@
+﻿using CassieCustard.Cassandra.Core;
+using System;
+namespace CassieCustard.Template.DataAccess
+{
+    public class BusinessModel_1 : ICassandraEntity
+    {
+        public Guid Id { get; set; }
+        public string BusinessModelName { get; set; }
+
+        public BusinessModel_1()
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ Cassandra.Template.DataAccess/Entities/BusinessModel_2.cs
@@ -0,0 +1,14 @@
+﻿using CassieCustard.Cassandra.Core;
+using System;
+namespace CassieCustard.Template.DataAccess
+{
+    public class BusinessModel_2 : ICassandraEntity
+    {
+        public Guid Id { get; set; }
+        public string BusinessModelName { get; set; }
+        public BusinessModel_2()
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
