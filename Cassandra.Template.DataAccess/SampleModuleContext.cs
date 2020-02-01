d934227cdc2f586736966bb28a5707ec4568f849
Wan <wmadzha11@hotmail.com>
2020-02-01 21:41:40 +0800

    Add Entities

---
+++ Cassandra.Template.DataAccess/SampleModuleContext.cs
@@ -0,0 +1,19 @@
+﻿using CassieCustard.Cassandra.Core;
+namespace CassieCustard.Template.DataAccess
+{
+    public class SampleModuleContext : CassandraContext
+    {
+        public SampleModuleContext(CassandraSettings settings) : base(settings)
+        {
+
+        }
+        public ICassandraEntitySet<BusinessModel_2> Model2 { get; set; }
+        public ICassandraEntitySet<BusinessModel_1> Model1 { get; set; }
+        protected override void SetupEntites()
+        {
+            Model1 = new CassandraEntitySet<BusinessModel_1>(this._CassandraComponent, this._Settings.KeySpaceName);
+            Model2 = new CassandraEntitySet<BusinessModel_2>(this._CassandraComponent, this._Settings.KeySpaceName);
+        }
+    }
+  
+}
