0cb1b68718df02638b0591ac2aae1b84b0ab3589
Wan <wmadzha11@hotmail.com>
2020-02-04 06:45:05 +0800

    Add Some CRUD Example

--- CassieCustard.ConsoleApp/Program.cs
+++ CassieCustard.ConsoleApp/Program.cs
@@ -12,7 +12,14 @@ namespace CassieCustard.ConsoleApp
             // Add Sample Data
             AddSampleData_Model1(Context);
             // Query Data Against Cassandra
-            GetLatestData_Model1(Context);
+            Guid lastofthelist = GetLatestData_Model1(Context);
+            // Try Get Single Data From Cassanra
+            BusinessModel_1 singleData = GetSingleData(lastofthelist, Context);
+            // Try Update Single Data And Push To Cassandra
+            UpdateData(singleData, Context);
+            // Try Get Back The Updated Data Against Cassandra
+            singleData = GetSingleData(lastofthelist, Context);
+
             Console.ReadLine();
         }
         /// <summary>
@@ -46,13 +53,40 @@ namespace CassieCustard.ConsoleApp
         /// Get Latest Data For Model 1
         /// </summary>
         /// <param name="ctx">Cassandra Business Module Context</param>
-        static void GetLatestData_Model1(SampleModuleContext ctx)
+        static Guid GetLatestData_Model1(SampleModuleContext ctx)
         {
+            Guid lastOfList = Guid.Empty;
             Console.WriteLine("Get Data List" + Environment.NewLine);
             foreach (var a in ctx.Model1.GetAll())
             {
                 Console.WriteLine(a.Id + " : " + a.BusinessModelName);
+                lastOfList = a.Id;
             }
+            return lastOfList;
+        }
+        /// <summary>
+        /// Get Single Data Against Cassandra
+        /// </summary>
+        /// <param name="singleId">Model Id</param>
+        /// <param name="ctx">Data store context instance</param>
+        /// <returns></returns>
+        static BusinessModel_1 GetSingleData(Guid singleId , SampleModuleContext ctx)
+        {
+            Console.WriteLine("Get Single Data ( The Last One In List)" + Environment.NewLine);
+            var result = ctx.Model1.GetById(singleId);
+            Console.WriteLine(result.Id + " : " + result.BusinessModelName);
+            return result;
+        }
+        /// <summary>
+        /// Update Single Business Model Object
+        /// </summary>
+        /// <param name="data">Model 1 instance</param>
+        /// <param name="ctx">Data store context instance</param>
+        static void UpdateData(BusinessModel_1 data , SampleModuleContext ctx)
+        {
+            Console.WriteLine("Trying To Update Entity" + Environment.NewLine);
+            data.BusinessModelName = "UpdatedModelName";
+            ctx.Model1.Edit(data);
         }
     }
 }
